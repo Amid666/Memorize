cbfd2ec54791d1a479951e37786314aa1312e868
Amid Mamedov <zuzukakabuba@gmail.com>
2026-01-04 13:24:27 +0200

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Amid Mamedov on 04.01.2026.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
